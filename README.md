00525f3059808503240b8694dff7197b22b5d9ad
Daniel Meechan <daniel.meechan.1+git@gmail.com>
2020-02-29 14:05:23 +0000

    Render a circle to screen

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# big-foil
+# Big Foil
 2D strategy game where you're Big Foil trying to destroy the planet through climate change - written in Rust using WebAssembly
+
+### Getting Started
+
+1. Install Rust locally (follow [this](https://sourabhbajaj.com/mac-setup/Rust/) guide if you're on Mac)
+2. Clone this repository and cd into the folder
+3. Compile the project locally: `cargo run` (this might take a few mins)
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use ggez::event::{self, EventHandler};
+use ggez::graphics::*;
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+
 fn main() {
     println!("Hello, world!");
+
+    // Make a Context.
+    let (mut ctx, mut event_loop) = ContextBuilder::new("foil", "🔥 Big Foil")
+        .build()
+        .expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut game = GameState::new(&mut ctx);
+
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e),
+    }
+}
+
+struct GameState {
+    // Your state here...
+}
+
+impl GameState {
+    pub fn new(_ctx: &mut Context) -> GameState {
+        // Load/create resources such as images here.
+        GameState {
+            // ...
+        }
+    }
+}
+
+impl EventHandler for GameState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            DrawMode::fill(),
+            mint::Point2 { x: 200.0, y: 300.0 },
+            100.0,
+            0.1,
+            graphics::WHITE,
+        )?;
+        graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
+
+        // Draw code here...
+        graphics::present(ctx)
+    }
 }
